44c3d6fb16538f99bf02f7f041ac66e4c524f7b4
tktart <tonkow235@gmail.com>
2024-12-17 22:47:03 +0700

    i

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,24 +1,16 @@
-#include<iostream>
-using namespace std;
+//ขอให้เติมมาเฉพาะ function printO() โดยที่ไม่ต้องเติม function main() มาในช่องนี้
+#include <iostream>
+using namespace std ;
 
-//Write the function printO() here
-
-int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
+void printO(int N,int M){
+    if (N <= 0 || M<= 0){
+        cout << "Invalid input" << endl;
+    }else {
+        for(int i = 0; i < N ;i++){
+            for(int j = 0; j < M; j++){
+                cout <<  "O";
+            }
+            cout << endl ;
+        }
+    }
 }
