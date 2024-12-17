bf67115af665f3aa4a25c1944941d7d1d95b2bc5
tktart <tonkow235@gmail.com>
2024-12-17 22:45:42 +0700

    o

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double a, b, c;
 	cout << "Enter initial loan: ";
+	cin >> a;
 	cout << "Enter interest rate per year (%): ";
+	cin >> b;
 	cout << "Enter amount you can pay per year: ";
+	cin >> c ;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +22,29 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+    cout << fixed << setprecision(2);
+
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	int year = 1;
+	while(a > 0){
+	     double interest = a*(b/100.0);
+	     double total = a + interest ;
+	     double payment = (total < c) ? total : c ;
+	     double ad = total - payment ;
+	
+	    
+    	 cout << setw(13) << left << year; 
+	     cout << setw(13) << left << a;
+	     cout << setw(13) << left << interest;
+	     cout << setw(13) << left << total;
+	     cout << setw(13) << left << payment;
+	     cout << setw(13) << left << ad;
+	     cout << "\n";	
+	
+	     a = ad;
+	     year ++;
+	}
 	
 	return 0;
 }
